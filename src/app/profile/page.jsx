4a2942c8a73f5ddfcc1904e3ad7a4e753848d7e8
Nayan Dey <ndeynayan123@gmail.com>
2026-04-30 03:22:24 +0530

    update user profile and profile data

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,32 @@
+"use client"
+
+import UpdateUserModal from "@/components/UpdateUserModal";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Card } from "@heroui/react";
+
+
+const ProfilePage = () => {
+
+    const userData = authClient.useSession()
+    console.log(userData)
+    const user = userData.data?.user;
+    // console.log(user);
+
+    return (
+        <div>
+            <Card className="max-w-96 mx-auto flex flex-col items-center border mt-10">
+                <Avatar className="h-20 w-20">
+                    <Avatar.Image alt={user?.image} referrerPolicy="no-referrer" />
+                    <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+                </Avatar>
+
+                <h2 className="text-xl font-bold ">{user?.name}</h2>
+                <p className="text-muted">{user?.email}</p>
+
+                <UpdateUserModal></UpdateUserModal>
+            </Card>
+        </div>
+    );
+};
+
+export default ProfilePage;
